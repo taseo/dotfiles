99c6c3469b3a6df1212c06c534b0dc070ea54390
taseo <aasaadesu@protonmail.ch>
2025-04-21 23:49:33 +0300

    feat: nvim config change
    - add ruff config
    - add format keymap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.set")
 require("config.keymaps")
 require("config.lazy")
+require("config.lsp")
 
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,10 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set(
+    "n",
+    "<leader>f",
+    function()
+        vim.lsp.buf.format()
+    end
+)
+
